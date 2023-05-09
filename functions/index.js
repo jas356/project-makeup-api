e6f6ec007178341b3ebc690cdf026f9b1516ec37
Jasmine Henry <Jasminehenry356@gmail.com>
2023-05-09 13:32:39 -0400

    Tested out one API endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
-const functions = require("firebase-functions");
+import express  from "express";
+import cors from "cors"
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const PORT = 3001
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+//My API points/ routes will be insterted below:
+app.get("/", (req, res) => {
+    res.send(`My api is working`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+})
